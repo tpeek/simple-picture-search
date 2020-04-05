a9abd37f0173a29558d54f26949281c436602e59
Tyler Peek (Seattle) <tpeek@us.imshealth.com>
2020-04-05 14:23:04 -0700

    add Pixabay logo

--- src/SavedArea.js
+++ src/SavedArea.js
@@ -24,6 +24,16 @@ const useStyles = makeStyles({
     width: 14,
     height: 14,
   },
+  pixabayLink: {
+    margin: '3px 15px 5px 0',
+    fontSize: 12,
+    lineHeight: 1.7,
+    color: '#555',
+    display: 'block',
+    float: 'left',
+    padding: '9px 12px 6px',
+    border: '1px solid #ccc',
+  },
 });
 
 const SavedArea = ({ savedResults }) => {
@@ -31,6 +41,14 @@ const SavedArea = ({ savedResults }) => {
 
   return (
     <div className={classes.root}>
+      <a href="https://pixabay.com/">
+        <img
+          src="https://pixabay.com/static/img/public/medium_rectangle_a.png"
+          alt="Pixabay"
+          target="_blank"
+          width={150}
+        />
+      </a>
       <h2>Saved</h2>
       {savedResults.map((result) => (
         <Link key={result.id} target="_blank" href={result.pageURL} className={classes.link}>
